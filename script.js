1c1f375d527fe4f1a3a6f314286b704d05d4a380
Rahul-R-K <83860417+Rahul-R-K@users.noreply.github.com>
2021-07-30 12:26:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ display=()=>{
     getData()
 }
 async function getData(){
-   try { num = document.getElementsByClassName('myText')[0].value;
+    try{num = document.getElementsByClassName('myText')[0].value;
     let result=await fetch("https://hacker-news.firebaseio.com/v0/item/"+num+".json?print=pretty")
     let data=await result.json();
-    console.log(data);
 
     let by=data.by; let id=data.id; let title=data.title; let type=data.type; let url=data.url
     
       console.log(by,id,title,type,url)  
       document.getElementsByClassName('main')[0].innerHTML="BY-"+by;
       document.getElementsByClassName('main')[1].innerHTML="TITLE -"+title;
-      document.getElementsByClassName('main')[2].innerHTML="TYPE -"+score;
+      document.getElementsByClassName('main')[2].innerHTML="TYPE -"+type;
       document.getElementsByClassName("main")[3].innerHTML=`<a class="ancher" href=${data.url}>URL-${data.url}</a>`;
-    }
-    catch{
-        alert("Enter valid ID")
-     }
+}
+catch{
+  alert("ENTER VALID ID")
+}
     }
